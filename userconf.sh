cb3cc16838a2ce48c666dfef4723d4888f8ae29b
robertheiberger <51130794+robertheiberger@users.noreply.github.com>
2020-02-26 16:25:03 -0500

    Update userconf.sh

--- userconf.sh
+++ userconf.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/with-contenv bash
 
 ## Set defaults for environmental variables in case they are undefined
-USER=${USER:=f404466}
-PASSWORD=${PASSWORD:=f404466}
+USER=${USER:=FILLIN}
+PASSWORD=${PASSWORD:=FILLIN}
 USERID=${USERID:=1000}
 GROUPID=${GROUPID:=1000}
 ROOT=${ROOT:=FALSE}
